887e3135f6bff2b58507c7c330df95171866caee
doziestar <chidosiky2015@gmail.com>
2022-03-16 12:17:55 +0100

    setting heroku

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="B94wq2UFiQkhJqBF8zN2DDEyeh6FzPOQiHpG35h4k2PXhYyDNZaYabMPOx6AGk8a",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "hezender.herokuapp.com"]
 
 # CACHES
 # ------------------------------------------------------------------------------
